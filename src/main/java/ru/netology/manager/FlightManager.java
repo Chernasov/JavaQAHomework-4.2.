c097bff9396688f00ede1ce92bc788dcc8836214
Timofey Chernasov <timofeychernasov@gmail.com>
2021-09-26 20:45:13 +0300

    Added manager

---
+++ src/main/java/ru/netology/manager/FlightManager.java
@@ -0,0 +1,32 @@
+package ru.netology.manager;
+
+import ru.netology.domain.CaseFlight;
+import ru.netology.repository.CaseFlightRepository;
+
+import java.util.Arrays;
+
+public class FlightManager {
+    private CaseFlightRepository repository;
+
+    public FlightManager(CaseFlightRepository repository) {
+        this.repository = repository;
+    }
+
+    public void add(CaseFlight item) {
+        repository.save(item);
+    }
+
+    public CaseFlight[] searchAll(String flightFrom, String flightTo) {
+        CaseFlight[] result = new CaseFlight[0];
+        for (CaseFlight caseFlight : repository.findAll()) {
+            if (caseFlight.getFlightIn() == flightTo && caseFlight.getFlightOut() == flightFrom) {
+                CaseFlight[] tmp = new CaseFlight[result.length + 1];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                tmp[tmp.length - 1] = caseFlight;
+                result = tmp;
+            }
+        }
+        Arrays.sort(result);
+        return result;
+    }
+}
