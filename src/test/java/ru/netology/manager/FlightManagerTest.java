630d9bfd6eec4bcb161705c8f8591e54185e89cd
Timofey Chernasov <timofeychernasov@gmail.com>
2021-09-26 22:30:34 +0300

    Added all tests

--- src/test/java/ru/netology/manager/FlightManagerTest.java
+++ src/test/java/ru/netology/manager/FlightManagerTest.java
@@ -12,21 +12,52 @@ public class FlightManagerTest {
     private FlightManager manager =new FlightManager(repository);
     private CaseFlight first = new CaseFlight(1, 1299, "SVO", "KZN", 95);
     private CaseFlight second = new CaseFlight(2, 2199, "VKO", "KZN", 95);
-    private CaseFlight third = new CaseFlight(3, 2385, "VKO", "KZN", 95);
+    private CaseFlight third = new CaseFlight(3, 2385, "VKO", "KZN", 91);
     private CaseFlight fourth = new CaseFlight(4, 3100, "DME", "KZN", 95);
+    private CaseFlight fifth = new CaseFlight(5, 3101, "DME", "UFA", 120);
+    private CaseFlight sixth = new CaseFlight(6, 3201, "VKO", "UFA", 120);
+    private CaseFlight seventh = new CaseFlight(7, 3209, "SVO", "UFA", 120);
+    private CaseFlight eighth = new CaseFlight(8, 3215, "SVO", "UFA", 110);
+    private CaseFlight nineth = new CaseFlight(9, 3415, "SMO", "UFA", 110);
 
     @BeforeEach
     public void setUp() {
+        manager.add(fifth);
         manager.add(first);
+        manager.add(eighth);
         manager.add(fourth);
+        manager.add(seventh);
         manager.add(third);
+        manager.add(sixth);
+        manager.add(nineth);
         manager.add(second);
     }
 
     @Test
-    public void searchAll() {
-        CaseFlight[] actual = manager.searchAll("VKO", "KZN");
-        CaseFlight[] expected = new CaseFlight[] {second, third};
+    public void sholdSearchAllWithFlightOutAndFlightIn() {
+        CaseFlight[] actual = manager.searchAll("SVO", "UFA");
+        CaseFlight[] expected = new CaseFlight[] {seventh, eighth};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchAllWithFlightOutAndWithoutFlightIn() {
+        CaseFlight[] actual = manager.searchAll("SVO", "WED");
+        CaseFlight[] expected = new CaseFlight[] {};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchAllWithoutFlightOutAndWithFlightIn() {
+        CaseFlight[] actual = manager.searchAll("RJC", "UFA");
+        CaseFlight[] expected = new CaseFlight[] {};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchAllWithoutFlightOutAndWithoutFlightIn() {
+        CaseFlight[] actual = manager.searchAll("RJC", "WMD");
+        CaseFlight[] expected = new CaseFlight[] {};
         assertArrayEquals(expected, actual);
     }
 }
